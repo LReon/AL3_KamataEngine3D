cbffa7947952c491366226fe97d37f855137e656
Rio-Fujiwara <k023g0030@g.neec.ac.jp>
2024-04-25 12:04:41 +0900

    ggg

--- DirectXGame/scene/GameScene.cpp
+++ DirectXGame/scene/GameScene.cpp
@@ -1,19 +1,79 @@
 #include "GameScene.h"
 #include "TextureManager.h"
 #include <cassert>
+#include "ImGuiManager.h"
+#include "PrimitiveDrawer.h"
 
 GameScene::GameScene() {}
 
-GameScene::~GameScene() {}
+GameScene::~GameScene() { 
+	delete sprite_; 
+	delete model_;
+}
+
 
 void GameScene::Initialize() {
 
 	dxCommon_ = DirectXCommon::GetInstance();
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
+	// ファイル名を指定してテクスチャ手を読み込む
+	textureHandle_ = TextureManager::Load("mario.jpg");
+	// スプライトの生成
+	sprite_ = Sprite::Create(textureHandle_, {100, 50});
+	// ファイル名を指定してテクスチャを読み込む
+	textureHandle_ = TextureManager::Load("mario.jpg");
+	//3Dモデルの生成
+	model_ = Model::Create();
+	// ワールドトランスフォームの初期化
+	worldTransform_.Initialize();
+	// ビュープロジェクションの初期化
+	viewProjection_.Initialize();
+	// サウンドデータの読み込み
+	soundHandle_ = audio_->LoadWave("fanfare.wav");
+	// 音声再生
+	//audio_->PlayWave(soundHandle_);
+	// 音声再生
+	voiceHandle_ = audio_->PlayWave(soundHandle_, true);
+	// ライン描画が参照するビュープロジェクションを指定する
+	PrimitiveDrawer::GetInstance()->SetViewProjection(&viewProjection_);
+
 }
 
-void GameScene::Update() {}
+void GameScene::Update() {
+	// スプライトの今の座標を取得
+	Vector2 position = sprite_->GetPosition();
+	// 座標を{2,1}移動
+	position.x += 2.0f;
+	position.y += 1.0f;
+	// 移動した座標をスプライトに反映
+	sprite_->SetPosition(position);
+	// スペースキーをを押した瞬間
+	if (input_->TriggerKey(DIK_SPACE)) {
+		// 音声停止
+		audio_->StopWave(voiceHandle_);
+	}
+	//デバッグテキストの表示
+	//ImGui::Text("Kamata Tarou %d.%d.%d", 2050, 12, 31);
+	ImGui::Begin("Debug1");
+	ImGui::Text("Kamata Tarou %d.%d.%d", 2050, 12, 31);
+	ImGui::End();
+
+	ImGui::Begin("Debug1");
+	// float3入力ボックス
+	ImGui::InputFloat3("InputFloat3", inputFloat3);
+	// float3スライダー
+	ImGui::SliderFloat3("SliderFloat3", inputFloat3, 0.0f, 1.0f);
+	ImGui::End();
+	// デモウィンドウの表示を有効化
+	ImGui::ShowDemoWindow();
+
+#ifdef _DEBUG
+	ImGui::Text("Kamata Tarou %d.%d.%d", 2050, 12, 31);
+#endif // 
+
+
+}
 
 void GameScene::Draw() {
 
@@ -42,7 +102,11 @@ void GameScene::Draw() {
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
 
+	model_->Draw(worldTransform_, viewProjection_, textureHandle_);
+
 	// 3Dオブジェクト描画後処理
+	// ラインを描画する
+	PrimitiveDrawer::GetInstance()->DrawLine3d({0, 0, 0}, {0, 10, 0}, {1.0f, 1.0f, 1.0f});
 	Model::PostDraw();
 #pragma endregion
 
@@ -53,7 +117,7 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに前景スプライトの描画処理を追加できる
 	/// </summary>
-
+	sprite_->Draw();
 	// スプライト描画後処理
 	Sprite::PostDraw();
 
