8905c59b281de86ff800212cba4b65c33baefa95
Rio-Fujiwara <k023g0030@g.neec.ac.jp>
2024-06-20 11:59:03 +0900

    m

--- DirectXGame/Player.h
+++ DirectXGame/Player.h
@@ -1,2 +1,27 @@
-#pragma once
-class Player {};
+##include<Model.h>
+#include <WorldTransform.h>
+
+    // enum clsss LRDirection {
+    //	kRight,
+    //
+    // };
+
+    class Player {
+public:
+	void Initialize(Model* model, ViewProjection* viewProjection, const Vector3& position);
+	void Update();
+	void Draw();
+	static inline const float kAcceleration = 1.0f;
+	Vector3 velocity_ = {};
+
+private:
+	// ワールド変換データ
+	WorldTransform worldTransform_;
+	// モデル
+	Model* modelPlayer_ = nullptr;
+
+	// テクスチャハンドル
+	uint32_t textureHandle_ = 0u;
+
+	ViewProjection* viewProjection_ = nullptr;
+};
